5086185bf7d145f38bea68cc678c4f1dba1460a9
Dave Dixon <dave.dixon@creditkarma.com>
2019-12-28 14:55:26 -0800

    Clean up examples.

--- examples/clj/infix.clj
+++ examples/clj/infix.clj
@@ -1,9 +1,9 @@
 (ns infix.clj
-  (:require [hash-meta.core :as ht :refer [defreader]]
-            [clojure.pprint :refer [pprint]]))
+  (:require [hash-meta.core :as ht :refer [defreader]]))
 
-(defreader i (fn [f]
-               (let [[v1 op v2] f]
-                 `(~op ~v1 ~v2))))
+(defreader i 
+  (fn [f]
+    (let [[v1 op v2] f]
+      `(~op ~v1 ~v2))))
 
 #i (2 + 3)
--- examples/clj/reader_vs_hash.clj
+++ examples/clj/reader_vs_hash.clj
@@ -2,15 +2,20 @@
   (:require [hash-meta.core :as ht :refer [defhashtag defreader]]
             [clojure.pprint :refer [pprint]]))
 
-(defn pp-fn
-  [f f']
-  `(let [x# ~f]
-     (pprint {:result x#
-              :form '~f'})
-     x#))
+(defreader reader-p
+  (fn [f]
+    `(let [x# ~f]
+       (pprint {:result x#
+                :form '~f})
+       x#)))
 
-(defreader reader-p #(pp-fn % %))
-(defhashtag hashtag-p pp-fn)
+(defhashtag hashtag-p
+  (fn [f f']
+    `(let [x# ~f]
+       (pprint {:result x#
+                :form '~f'})
+       x#)))
+
 
 (inc #reader-p (* 2 #reader-p (+ 3 #reader-p (* 4 5))))
 (inc #hashtag-p (* 2 #hashtag-p (+ 3 #hashtag-p (* 4 5))))
