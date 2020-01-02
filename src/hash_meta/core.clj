e58f34d99cd42a7e3efaf7c00c67ba4b6af0cefc
Dave Dixon <dave.dixon@creditkarma.com>
2020-01-02 12:37:46 -0800

    Fix doc strings.

--- src/hash_meta/core.clj
+++ src/hash_meta/core.clj
@@ -36,14 +36,15 @@
 (defmacro defreader-n
   "Defines and registers a \"nestable tagged literal\" reader macro which will transform
    the tagged form and hide the effects of nested hashtag expansions.
-      * id - the name of the tag, e.g. p -> #p, foo/bar -> #foo/bar.
-      * transform - a 2 argument function. This function is used at macro-expansion,
+      * id - the name of the tag, e.g. p -> #p.
+      * transform - a 3 argument function. This function is used at macro-expansion,
         so the usual macro rules apply. The first argument is the form
         as seen by the compiler, including effects of nested hashtag
         expansions. The second is the \"original\" form, as it would be seen
         before expansion (and without any hashtags). Another way to think about
         it is that the first form is the one you want to execute, while the
-        second is for display."
+        second is for display. The third argument is any metadata associated
+        with the tagged form."
   [id transform]
   (make-reader id transform true))
 
@@ -51,7 +52,8 @@
   [id transform]
   "Defines and registers a \"tagged literal\" reader macro which will transform
    the tagged form.
-      * id - the name of the tag, e.g. p -> #p, foo/bar -> #foo/bar.
-      * transform - a 1 argument function. This function is used at macro-expansion,
-        so the usual macro rules apply. The argument is the tagged form."
+      * id - the name of the tag, e.g. p -> #p.
+      * transform - a 2 argument function. This function is used at macro-expansion,
+        so the usual macro rules apply. The first argument is the tagged form.
+        The second argument is any metadata associated with the tagged form."
   (make-reader id transform false))
